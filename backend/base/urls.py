0903ae0a148ecafff081adfefde23d641e0ad92d
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-05-16 11:29:54 +0600

    backend user authentication, permission_classes

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('products/', views.getProducts, name='products'),
     path('product/<pk>/', views.getProduct, name='product'),
+    path('users/profile/', views.getUserProfile, name='users'),
+    path('users/', views.getUsers, name='users'),
 ]
+
