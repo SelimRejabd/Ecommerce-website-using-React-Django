f4032e1b0c7e83ac81d0b6a9ca19357fcda8a41f
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-05-16 11:55:35 +0600

    user registration

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.registerUser, name='register'),
     path('products/', views.getProducts, name='products'),
     path('product/<pk>/', views.getProduct, name='product'),
     path('users/profile/', views.getUserProfile, name='users'),
