df601c4d01722f4680046d5510e93c74cdbf6e90
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-06-07 19:58:13 +0600

    order views error solved

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
+from datetime import datetime
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -58,7 +59,6 @@ def addOrderItems(request):
 @permission_classes([IsAuthenticated])
 def getOrderById(request, pk):
     user = request.user
-
     try:
         order = Order.objects.get(_id=pk)
         if user.is_staff or order.user == user:
@@ -66,8 +66,16 @@ def getOrderById(request, pk):
             return Response(serializer.data)
         else:
             return Response({'detail': 'Not authorized to view this order'}, status=status.HTTP400_BAD_REQUEST)
-    
-    except:
-        return Response({'detail': 'Order does not exist'}, status=status.HTTP400_BAD_REQUEST)
+    except Order.DoesNotExist:
+        return Response({'detail': 'Order does not exist'}, status=status.HTTP404_NOT_FOUND)
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request, pk):
+    order = Order.objects.get(_id=pk)
 
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+
+    return Response('Order was paid')
